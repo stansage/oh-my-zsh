aa15422a4b01c29b3498aa54d14da7805d831cd6
stansage <me@stansage.com>
2014-04-30 22:36:06 +0700

    This is my patch.

--- lib/aliases.zsh
+++ lib/aliases.zsh
@@ -8,7 +8,6 @@ alias -- -='cd -'
 
 # Super user
 alias _='sudo'
-alias please='sudo'
 
 #alias g='grep -in'
 
@@ -17,10 +16,14 @@ alias history='fc -l 1'
 
 # List direcory contents
 alias lsa='ls -lah'
-alias l='ls -lA1'
+alias l='ls -la'
 alias ll='ls -l'
-alias la='ls -lA'
 alias sl=ls # often screw this up
 
 alias afind='ack-grep -il'
 
+alias apt-I='sudo apt-get install'
+alias apt-S='apt-cache search'
+alias o='gnome-open'
+alias adb='/media/home/share/android-sdk/platform-tools/adb'
+alias my-ip='wget -q -O- http://goobotool.appspot.com/myip | head -n1'
