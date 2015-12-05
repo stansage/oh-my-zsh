93c38b4a972aaecdd766a332b1a97585a72ff9ab
stansage <me@stansage.com>
2015-12-05 06:42:23 +0600

    added emeu command

--- plugins/gentoo/gentoo.plugin.zsh
+++ plugins/gentoo/gentoo.plugin.zsh
@@ -6,14 +6,18 @@ if [[ -x `which emerge` ]]; then
 
 	# emerge - https://dev.gentoo.org/~zmedico/portage/doc/man/emerge.1.html
 	#alias eupg='sudo pacman -Syu'        # Synchronize with repositories before upgrading packages that are out of date on the local system.
+
+	alias emeu='sudo  emerge --ask --update --newuse @world'
 	alias emei='sudo emerge --ask'           # Install specific package(s) from the repositories
 
 fi
 
 # Look for equery and add some useful functions if we have it.
 if [[ -x `which equery` ]]; then
+
 	alias emeqm='equery m'
 	alias emeqf='equery f'
+
 fi
 
 #alias pacins='sudo pacman -U'          # Install specific package not from the repositories but from a file
